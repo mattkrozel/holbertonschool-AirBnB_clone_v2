b166366a6a028abb7a87b349f57e7fad373d22e7
Matt Krozel <mattkrozel@gmail.com>
2023-10-27 10:12:51 -0700

    added more class attributes

--- models/place.py
+++ models/place.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
+
 
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+    city_id = Column(String(60), nullable=False)
+    user_id = Column(String(60), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column()
+    number_bathrooms = Column()
+    max_guest = Column()
+    price_by_night = Column(Integer)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []

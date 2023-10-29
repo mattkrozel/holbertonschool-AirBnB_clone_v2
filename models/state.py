4f202d4a3fa1071cda9922762846fe3e8441965c
Matt Krozel <mattkrozel@gmail.com>
2023-10-29 12:39:00 -0700

    finishing up tasks for adding class atrributes, created getter atttribute to return cities

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from models.city import City
+import os
 
 
 class State(BaseModel, Base):
@@ -11,3 +13,16 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
                           cascade="all, delete-orphan")
+    
+    @property
+    def cities(self):
+        '''
+        getter attribute for cities
+        '''
+        from models import storage
+        citylist = []
+        allcities = storage.all(City).values()
+        for city in allcities:
+            if self.id == city.state_id:
+                citylist.append(city)
+        return citylist

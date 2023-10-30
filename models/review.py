684b930f0151e0ce4c0629245bc6e74c054f3485
Matt Krozel <mattkrozel@gmail.com>
2023-10-30 09:09:53 -0700

    fixed classes for instance of file storagfe and not db, also added import os to some class files

--- models/review.py
+++ models/review.py
@@ -3,11 +3,18 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = "reviews"
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)   
+    else:
+        __tablename__ = "reviews"
+        place_id = ""
+        user_id = ""
+        text = ""

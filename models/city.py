434d15669128e54dec25829c6b5e4c1b2b11bc22
Matt Krozel <mattkrozel@gmail.com>
2023-10-30 08:54:40 -0700

    push for class file upodates, more fixes to if else statements

--- models/city.py
+++ models/city.py
@@ -15,6 +15,6 @@ class City(BaseModel, Base):
         places = relationship("Place", backref="cities", cascade="all, delete")
     else:
         __tablename__ = "cities"
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
+        state_id = ""
+        name = ""
         places = relationship("Place", backref="cities", cascade="all, delete")

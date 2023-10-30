2a2b4760a0cf4807ede8fe58334b488f6698c079
Matt Krozel <mattkrozel@gmail.com>
2023-10-30 08:36:10 -0700

    push for class file upodates, implemented if else for databse or not

--- models/state.py
+++ models/state.py
@@ -9,20 +9,25 @@ import os
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
-    
-    @property
-    def cities(self):
-        '''
-        getter attribute for cities
-        '''
-        from models import storage
-        citylist = []
-        allcities = storage.all(City).values()
-        for city in allcities:
-            if self.id == city.state_id:
-                citylist.append(city)
-        return citylist
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")   
+    else:
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")   
+        @property
+        def cities(self):
+            '''
+            getter attribute for cities
+            '''
+            from models import storage
+            citylist = []
+            allcities = storage.all(City).values()
+            for city in allcities:
+                if self.id == city.state_id:
+                    citylist.append(city)
+            return citylist

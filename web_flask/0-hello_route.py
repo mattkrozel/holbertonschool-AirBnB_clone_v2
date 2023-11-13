a0e58dff51b23d69a96c2b7e22b47169a0588882
Matt Krozel <mattkrozel@gmail.com>
2023-11-13 13:17:49 -0800

    first push for task 0, hello hbnb

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,10 +2,12 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def prompt():
+def hello():
     return ('Hello HBNB!')
 
 if __name__ == '__main__':

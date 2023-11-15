fb625c582d0b5e5823b51e6b0aa9da0eb04fb11d
Matt Krozel <mattkrozel@gmail.com>
2023-11-15 12:30:34 -0800

    module doc fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''script opens flask server with list of states'''
+''' script opens flask server with list of states '''
 from flask import Flask
 from flask import render_template
 from models import storage, State
@@ -10,18 +10,14 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    '''
-    displays html page
-    '''
+    ''' displays html page '''
     return render_template('7-states_list.html',
                            states=storage.all(State))
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    '''
-    removes session
-    '''
+    ''' removes session '''
     storage.close()
 
 

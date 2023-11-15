554c26c51e07b1a26b162dc837402b00576c224c
Matt Krozel <mattkrozel@gmail.com>
2023-11-15 12:50:10 -0800

    module doc fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that starts web flask application on port 5000 fetching from storage engine """
+"""script that starts web flask application on port 5000 fetching from storage engine"""
 from flask import Flask
 from flask import render_template
 from models import storage, State
@@ -10,12 +10,14 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """display html page with states list"""
     return render_template('7-states_list.html',
                            states=storage.all(State))
 
 
 @app.teardown_appcontext
 def teardown(exc):
+    """closes out or tears down"""
     storage.close()
 
 
